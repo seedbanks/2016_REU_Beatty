370d907913a9cf0b0f10586ccd316882909dd649
JayCBeatty <jcbeatty17@transy.edu>
2016-07-21 14:56:28 -0400

    Uploaded Code

---
+++ O'Toole'sBiofilmAssay Analysis.R
@@ -0,0 +1,42 @@
+#Residence Time Chemostat Experiment
+#Biofilm Production Correlation Analysis
+#Jaylen C. Beatty, July 19th, 2016, Indiana University, Lennon Lab REU
+film=read.csv(file.choose(), header = TRUE)
+str(film)
+names(film)
+summary(film)
+attach(film)
+shapiro.test(RawAbsorb)#p > .05, data nonparametric. Use Spearman's or transform
+#R.T. values are normally distributed.
+logrt= log(Restime)#Graph region cannot appropriately capture the wide gradient of R.T. values. Transformation makes this clearer.
+logabsorb=log(RawAbsorb)
+shapiro.test(logabsorb)
+plot(logrt, ModAbsorb, xlab= "Log Residence Time (hours)", ylab= "Absorbance Readings", main = "Biofilm Production Is Not Correlated with Res. Time",  cex= .8)
+text(x= ('3.00'), y=.30, labels='p > .05')
+(text(x=('3.00'), y= .25, labels= 'r= -.032133'))
+abline(lm(ModAbsorb ~ logrt))
+regfilm= lm(formula= logabsorb ~ logrt)
+cor.test(ModAbsorb, logrt, method="pearson")# p > .05, suggesting there is no correlation.
+summary(regfilm)
+
+#test data with controls subtracted
+shapiro.test(ModAbsorb)# p > .05, data nonparametric
+plot(logrt, ModAbsorb)
+abline(lm(ModAbsorb ~ logrt))
+cor.test(ModAbsorb, logrt, method="spearman")
+regfilm= lm(formula= ModAbsorb ~ logrt)
+summary(regfilm)
+
+recabsorb= (1/ModAbsorb)
+plot(logrt, recabsorb)
+shapiro.test(recabsorb)
+abline(lm(recabsorb ~ logrt))
+sqrtabsorb= sqrt(ModAbsorb)
+shapiro.test(sqrtabsorb)
+sqrtrestime= sqrt(Restime)
+shapiro.test(sqrtrestime)
+
+allelofilm= aov(sqrtabsorb ~ Chemostat, data= film)
+summary(allelofilm)
+TukeyHSD(allelofilm)
+boxplot(ModAbsorb ~ Chemostat, data= film, main= "Biofilm Production Comparison" , xlab= "Chemostat # ", ylab= " Absorbance Readings", cex.lab= .7)
